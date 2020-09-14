d62a61fcfc0b40cca3984bb65e75d811c73afc34
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2020-09-14 23:26:13 +0530

    create an add method to the context store

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,10 +3,16 @@ import { MovieContext } from '../context/MovieContext'
 
 const Nav = () => {
 	const [movies, setMovies] = useContext(MovieContext)
+
+	const addMovie = () => {
+		setMovies(prevMovies => [...prevMovies, { name: 'test', price: '$05', id: '002' }])
+	}
+
 	return (
 		<div>
 			<h3>PHOENIXXX</h3>
 			<p>List of Movies: {movies.length}</p>
+			<button onClick={addMovie}>Add movie</button>
 		</div>
 	)
 }
